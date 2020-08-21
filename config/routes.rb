250ebc6e295bea879f366a4d8a58bf8196a52915
xblueoceanx <ginnnokizuna@yahoo.co.jp>
2020-08-21 19:58:08 +0900

    [add]いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/about'
   resources :books, only: [:new, :create, :index, :show, :edit, :destroy, :update] do
+  	resource :favorites, only: [:create, :destroy]
   	resource :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update, :index]

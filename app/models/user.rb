3cdb02a676f52293ba5098844dcc04bcb2784e3a
xblueoceanx <ginnnokizuna@yahoo.co.jp>
2020-08-23 18:28:43 +0900

    [add]フォロー機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,31 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
- validates :name, presence: true, length: {minimum: 2, maximum: 20 }
- validates :introduction, length: {maximum: 50 }
+  validates :name, presence: true, length: {minimum: 2, maximum: 20 }
+  validates :introduction, length: {maximum: 50 }
 
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人
+  has_many :follower_user, through: :followed, source: :follower # 自分をフォローしている人
   attachment :profile_image
+
+  # ユーザーをフォローする
+def follow(user_id)
+  follower.create(followed_id: user_id)
+end
+
+# ユーザーのフォローを外す
+def unfollow(user_id)
+  follower.find_by(followed_id: user_id).destroy
+end
+
+# フォローしていればtrueを返す
+def following?(user)
+  following_user.include?(user)
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   end
   resources :users, only: [:show, :edit, :update, :index]
   root "home#top"
+  post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
